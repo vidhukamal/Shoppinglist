020d197e1ab25f7e841f9b8607f7be571f42dcc7
vidhukamal <vidhukamal@gmail.com>
2013-10-21 06:22:09 -0400

    Did lot of modifications with adding ,striking and removing the items

--- code.js
+++ code.js
@@ -1,12 +1,29 @@
 $(document).ready(function(){
+	var imgTag = "<img src=\"remove.png\" class=\"remove\"/>";
 	$('form').on('submit',function(){
 		var item = $('#add_item').val();
-		$(".product").append("<label><input type=checkbox id=shop_item>"+item+"</label>")
-		return false;
-	});
+		//alert(item);
+		$('ul').append("<li id="+ item +" class='shop_item'><input type='checkbox' class='check_cls' />" + item + imgTag+"</li>");
+		
+		$("li.shop_item").on("click",function(){
+			//alert($(this).html());
+			var myval = $(this).attr('id');
+    		if ($('.check_cls').is(":checked")) {
+        		$(this).html("<input type='checkbox' class='check_cls' checked/><strike>" + myval + "</strike>"+imgTag);
+	    	} 
+    		else 
+    		{
+    			$(this).html("<input type='checkbox' class='check_cls'/>" + myval + imgTag);
+	    	}
+    		return false;
+		});
+		
+		
+		$('.remove').on("click", function(){
+			$(this).parent().remove();			
+		});	
 	
-	$('#shop_item').click(function(){
-		$("label").style.textDecoration = 'line-through';
+		return false;
+		
 	});
-	 
 });
